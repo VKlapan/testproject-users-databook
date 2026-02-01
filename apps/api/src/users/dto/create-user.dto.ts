87f05e4cfec3426ab136e71862c80bbf51a0addc
Volodymyr Klapan <volodymyr.klapan@gmail.com>
2026-02-01 18:21:38 +0200

    added dto validation

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
-    name: string;
-    email: string;
-    phone: string;
-    birthday: string;
+  @IsString()
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsEmail({}, { message: 'Email is invalid' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Phone number is required' })
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Birthday is required' })
+  birthday: string;
 }
